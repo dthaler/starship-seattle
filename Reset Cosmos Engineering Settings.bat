ecb43a7ab5da020d936213b92c352bc2309d53b8
Dave Thaler <dthaler1968@gmail.com>
2026-08-24 10:54:16 -0700

    Update README file with a layout overview
    Fixes #7
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- Reset Cosmos Engineering Settings.bat
+++
@@ -1,2 +0,0 @@
-copy C:\Users\artem\OneDrive\Programs\Cosmos-1.3.0\data\Engineering-Presets.txt.backup C:\Users\artem\OneDrive\Programs\Cosmos-1.3.0\data\Engineering-Presets.txt
-copy "C:\Users\artem\OneDrive\Programs\Cosmos-1.3.0 locked\data\Engineering-Presets.txt.backup" "C:\Users\artem\OneDrive\Programs\Cosmos-1.3.0 locked\data\Engineering-Presets.txt"
