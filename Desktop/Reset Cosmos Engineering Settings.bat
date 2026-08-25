79e68758faa8d8aff274bf3e1f59f14e29296d4e
Dave Thaler <dthaler1968@gmail.com>
2026-08-25 08:12:07 -0700

    Add files for the Big Red Button
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- Desktop/Reset Cosmos Engineering Settings.bat
+++ Desktop/Reset Cosmos Engineering Settings.bat
@@ -1,2 +1,4 @@
-copy "%OneDrive%\Programs\Cosmos-1.3.0\data\Engineering-Presets.txt.backup" "%OneDrive%\Programs\Cosmos-1.3.0\data\Engineering-Presets.txt"
-copy "%OneDrive%\Programs\Cosmos-1.3.0 locked\data\Engineering-Presets.txt.backup" "%OneDrive%\Programs\Cosmos-1.3.0 locked\data\Engineering-Presets.txt"
+set COSMOS_PARENT_DIR="%OneDrive%\Programs"
+
+copy "%COSMOS_PARENT_DIR%\Cosmos-1.3.0\data\Engineering-Presets.txt.backup" "%COSMOS_PARENT_DIR%\Cosmos-1.3.0\data\Engineering-Presets.txt"
+copy "%COSMOS_PARENT_DIR%\Cosmos-1.3.0 locked\data\Engineering-Presets.txt.backup" "%COSMOS_PARENT_DIR%\Cosmos-1.3.0 locked\data\Engineering-Presets.txt"
