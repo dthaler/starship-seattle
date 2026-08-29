b335bf5679f1de355191b0958477c8b277bc9c75
Dave Thaler <dthaler1968@gmail.com>
2026-08-28 21:54:36 -0700

    Add script to install files
    Fixes #15
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

---
+++ Cosmos/ssupdate.bat
@@ -0,0 +1,28 @@
+echo Updating Starship Seattle files...
+
+rem Determine profile name (default = harbinger)
+set PROFILE=%1
+if "%PROFILE%"=="" set PROFILE=harbinger
+
+rem Update missions to latest dev release
+cd data\missions
+call sbs update
+call sbs production -b v1.4.0_dev -q
+
+rem Back up settings.yaml
+copy LegendaryMissions\settings.yaml LegendaryMissions\settings-stock.yaml
+
+rem Create LegendaryMissionsUnlocked
+xcopy /s /y LegendaryMissions\ LegendaryMissionsUnlocked\
+
+rem Restore Starship Seattle configuration
+cd ..
+xcopy /s /y ..\StarshipSeattle-data\* .
+
+rem Back up preferences.json if not already done
+if not exist preferences-stock.json (
+    copy preferences.json preferences-stock.json
+)
+
+rem Install preferences.json
+copy preferences-%PROFILE%.json preferences.json
