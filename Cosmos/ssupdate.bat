37455bc941f5f190df58e141803135f83f893cf0
Dave Thaler <dthaler1968@gmail.com>
2026-08-29 08:28:07 -0700

    Fix preferences location
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- Cosmos/ssupdate.bat
+++ Cosmos/ssupdate.bat
@@ -4,18 +4,19 @@ rem Determine profile name (default = harbinger)
 set "PROFILE=%~1"
 if "%PROFILE%"=="" set "PROFILE=harbinger"
 
+rem Back up preferences.json if not already done
+cd data
+if not exist preferences-stock.json (
+    copy preferences.json preferences-stock.json
+)
+
 rem Update missions to latest dev release
-cd data\missions
+cd missions
 call sbs update
 call sbs production -b v1.4.0_dev -q
 
 rem Back up settings.yaml
 copy LegendaryMissions\settings.yaml LegendaryMissions\settings-stock.yaml
-
-rem Back up preferences.json if not already done
-if not exist preferences-stock.json (
-    copy preferences.json preferences-stock.json
-)
 
 rem Create LegendaryMissionsUnlocked
 xcopy /s /y LegendaryMissions\ LegendaryMissionsUnlocked\
