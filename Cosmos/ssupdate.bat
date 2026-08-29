18508d8142f94e69caf202b446292deeb35511c9
Dave Thaler <dthaler1968@gmail.com>
2026-08-29 08:31:17 -0700

    Allow configuring intrepid and artemis servers (#30)
    * Allow configuring intrepid and artemis servers
    
    Fixes #29
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>
    
    * Restore current directory at end of ssupdate
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>
    
    * Fix preferences location
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>
    
    ---------
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- Cosmos/ssupdate.bat
+++ Cosmos/ssupdate.bat
@@ -4,8 +4,14 @@ rem Determine profile name (default = harbinger)
 set "PROFILE=%~1"
 if "%PROFILE%"=="" set "PROFILE=harbinger"
 
+rem Back up preferences.json if not already done
+cd data
+if not exist preferences-stock.json (
+    copy preferences.json preferences-stock.json
+)
+
 rem Update missions to latest dev release
-cd data\missions
+cd missions
 call sbs update
 call sbs production -b v1.4.0_dev -q
 
@@ -16,13 +22,5 @@ rem Create LegendaryMissionsUnlocked
 xcopy /s /y LegendaryMissions\ LegendaryMissionsUnlocked\
 
 rem Restore Starship Seattle configuration
-cd ..
-xcopy /s /y ..\StarshipSeattle-data\* .
-
-rem Back up preferences.json if not already done
-if not exist preferences-stock.json (
-    copy preferences.json preferences-stock.json
-)
-
-rem Install preferences.json
-copy preferences-%PROFILE%.json preferences.json
+cd ..\..
+xcopy /s /y %PROFILE%-data\ data\
